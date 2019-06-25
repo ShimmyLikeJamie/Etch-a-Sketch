7864da96ed5ac161d2c1f0e92a0302f8b5ec1c61
shimmylikejimmy <jimmyoppelpersonal@gmail.com>
2019-06-24 21:22:07 -0700

    Added button to clear grid and choose the number of squares on each side

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Etch-a-Sketch
 Odin Project
+
+This project has you make a rudimentary "Etch-a-Sketch" in website form using DOM manipulation.
--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const container = document.querySelector("#container");
-console.log(container);
 
-for (let i = 0; i < (16*16); i++) {
-    container.appendChild(document.createElement("span"));
+
+
+function createGrid(num) {
+    const container = document.querySelector("#container");
+    
+    for (let i = 0; i < (num*num); i++) {
+        container.appendChild(document.createElement("span"));
+    }
+    container.style.gridTemplateColumns = "1fr ".repeat(num);
+    container.style.gridTemplateRows = "1fr ".repeat(num);
 }
 
+
+function addEvents() {
 const squares = document.querySelectorAll("span");
 
 squares.forEach(function(square) {
     square.setAttribute("class", "gridItem");
     square.addEventListener("mouseover", function () {
-        square.setAttribute("id", "on");
+        square.setAttribute("class", "gridItem, on");
     });
     square.addEventListener("click", function() {
-        square.setAttribute("id", "off");
+        square.setAttribute("class", "gridItem, off");
     })
 });
+}
+
+const newGridButton = document.querySelector("button");
+
+newGridButton.addEventListener("click", function() {
+    const boxes = document.querySelectorAll("span");
+    let squaresPerSide = parseInt(prompt("How many squares would you like per side?", 16));
+    boxes.forEach(function(box) {
+        container.removeChild(box);
+    });
+    createGrid(squaresPerSide);
+    addEvents();
+});
+
+createGrid(16);
+addEvents();
