10bbd9d35c81a07555ccd3d5d116319495e61b62
shimmylikejimmy <jimmyoppelpersonal@gmail.com>
2019-06-24 17:20:07 -0700

    Fixed grid display. Added color change on each grid item on mouse hover, and on click.

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const container = document.querySelector("#container");
 console.log(container);
 
-for (let i = 0; i < 16; i++) {
-    container.appendChild(document.createElement("div"));
+for (let i = 0; i < (16*16); i++) {
+    container.appendChild(document.createElement("span"));
 }
 
-const squares = document.querySelectorAll("div");
+const squares = document.querySelectorAll("span");
 
 squares.forEach(function(square) {
     square.setAttribute("class", "gridItem");
+    square.addEventListener("mouseover", function () {
+        square.setAttribute("id", "on");
+    });
+    square.addEventListener("click", function() {
+        square.setAttribute("id", "off");
+    })
 });
